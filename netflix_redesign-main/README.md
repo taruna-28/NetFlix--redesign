108cc76f6f5f57f96142e5761bbae9a25eb12c21
taruna-28 <126285576+taruna-28@users.noreply.github.com>
2025-07-14 17:30:17 +0530

    Netflix-Inspired
    Built a user-friendly movie streaming platform with login, search, and improved
    UI accessibility

---
+++ netflix_redesign-main/README.md
@@ -0,0 +1,23 @@
+# Netflix Clone #
+---
+<img src='/netflix_home.png' alt='logoapp'/>
+
+### About App ###
+
+Redesigned netflix using React, redux, axios and api integration with query searching
+<ul>
+  <li>enhanced color palette, code structured in components</li>
+  <li>routing established</li>
+   <li>store is sliced</li>
+  </ul>
+  
+  ---
+  
+ ### How To Use ###
+login using credentials given in my portfolio under projects section</b> explore different sets of movies with login logout functionality of firebase.
+
+### cloning the project ###
+you can simply download the zip file or clone it on your local device
+type <b> yarn </b> in cmd in project folder
+and then <b>yarn start</b>
+play around with it !
---
+++ netflix_redesign-main/src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { login, logout, selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "./firebase";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
+  return (
+    <div className="app">
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
